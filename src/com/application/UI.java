edf9278131d997bc99ced49213ce24b8b7703d98
Matheus Adams <adamsmatheus7@gmail.com>
2020-05-08 11:26:47 -0300

    Tabuleiro criado

--- src/com/application/UI.java
+++ src/com/application/UI.java
@@ -7,9 +7,11 @@ public class UI {
         for(int i=0;i<pieces.length;i++){
             System.out.print((8 - i) + " ");
             for( int j=0; j<pieces.length;j++){
-                printPiece();
+                printPiece(pieces[i][j]);
             }
+            System.out.println();
         }
+        System.out.println("  a b c d e f g h");
 
     }
     public static void printPiece(ChessPiece piece){
